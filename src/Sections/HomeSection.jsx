ce3f56e6e7a9d733304cc758182dfa2050c959cc
zirris512 <zirris512@gmail.com>
2022-01-05 10:54:19 -0600

    add social links component

--- src/Sections/HomeSection.jsx
+++ src/Sections/HomeSection.jsx
@@ -1,5 +1,6 @@
 import styles from "./HomeSection.module.scss";
 import profilePic from "../assets/profile-pic.jpg";
+import ContactLinks from "../components/ContactLinks/ContactLinks";
 
 const HomeSection = () => {
     return (
@@ -8,11 +9,16 @@ const HomeSection = () => {
                 <h1>Hello, my name is Brent.</h1>
             </div>
             <div className={styles.imageContainer}>
-                <img src={profilePic} alt="Profile picture" className={styles.profilePic} />
+                <img
+                    src={profilePic}
+                    alt="Profile picture"
+                    className={styles.profilePic}
+                />
             </div>
             <div className={styles.heroSubtitle}>
                 <h2>Full-Stack Developer</h2>
             </div>
+            <ContactLinks />
         </header>
     );
 };
