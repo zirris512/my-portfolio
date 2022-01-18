e3f48a1981aa6a4a4517989b200e0e036058daa1
zirris512 <zirris512@gmail.com>
2022-01-18 13:27:25 -0600

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>Brent Edwards &copy; {2022}</p>
+        </footer>
+    );
+};
+
+export default Footer;
