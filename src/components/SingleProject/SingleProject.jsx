cda2eace79cbd0a5c4c065dcba7e41602470431d
zirris512 <mina512@live.com>
2022-01-18 12:56:18 -0600

    add single project component and styling

--- src/components/SingleProject/SingleProject.jsx
+++ src/components/SingleProject/SingleProject.jsx
@@ -1,21 +1,44 @@
 import styles from "./SingleProject.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const SingleProject = ({ projectData }) => {
     const technologiesString = projectData.technologies.join(", ");
 
     return (
-        <div>
-            <div>
-                <img src={projectData.imgUrl} alt={projectData.imgAlt} />
+        <article className={styles.projectContainer}>
+            <div className={styles.projectImgContainer}>
+                <img
+                    src={projectData.imgUrl}
+                    alt={projectData.imgAlt}
+                    className={styles.projectImg}
+                />
             </div>
-            <div>
-                <h2>{projectData.name}</h2>
-                <p>{technologiesString}</p>
-                <p>{projectData.description}</p>
-                <a href={projectData.deployedUrl}></a>
-                <a href={projectData.github}></a>
+            <div className={styles.projectContent}>
+                <div>
+                    <h3>{projectData.name}</h3>
+                    <p className={styles.techList}>{technologiesString}</p>
+                </div>
+                <div>
+                    <p>{projectData.description}</p>
+                    <a
+                        href={projectData.deployedUrl}
+                        target="_blank"
+                        className={styles.projectLink}
+                    >
+                        <FontAwesomeIcon icon={faGlobe} />
+                    </a>
+                    <a
+                        href={projectData.github}
+                        target="_blank"
+                        className={styles.projectLink}
+                    >
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                </div>
             </div>
-        </div>
+        </article>
     );
 };
 
