6d91114342b06256996495fe724519fdb4081125
zirris512 <zirris512@gmail.com>
2023-04-26 20:38:26 -0500

    fix issue with image being sized too small on smaller screens

--- src/Sections/HomeSection.jsx
+++ src/Sections/HomeSection.jsx
@@ -6,33 +6,25 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import ImageLoading from "../components/ImageLoading/ImageLoading";
 
 const HomeSection = () => {
-    return (
-        <header className={styles.heroHeader}>
-            <div className={styles.heroTitle}>
-                <h1>
-                    Hello, I am <span className={styles.name}>Brent</span>.
-                </h1>
-            </div>
-            <div className={styles.imageContainer}>
-                <ImageLoading
-                    image={profilePic}
-                    imgAlt="profile picture"
-                    imgClass={styles.profilePic}
-                />
-            </div>
-            <div className={styles.heroSubtitle}>
-                <h2>Full-Stack Developer</h2>
-            </div>
-            <ContactLinks />
-            <a
-                className={styles.scrollDownIcon}
-                href="#about"
-                aria-label="scroll down button"
-            >
-                <FontAwesomeIcon icon={faChevronDown} />
-            </a>
-        </header>
-    );
+	return (
+		<header className={styles.heroHeader}>
+			<div className={styles.heroTitle}>
+				<h1>
+					Hello, I am <span className={styles.name}>Brent</span>.
+				</h1>
+			</div>
+			<div className={styles.imageContainer}>
+				<ImageLoading image={profilePic} imgAlt="profile picture" imgClass={styles.profilePic} />
+			</div>
+			<div className={styles.heroSubtitle}>
+				<h2>Full-Stack Developer</h2>
+			</div>
+			<ContactLinks />
+			<a className={styles.scrollDownIcon} href="#about" aria-label="scroll down button">
+				<FontAwesomeIcon icon={faChevronDown} />
+			</a>
+		</header>
+	);
 };
 
 export default HomeSection;
