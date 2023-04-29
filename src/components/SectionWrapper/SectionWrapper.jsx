4caeee963de65865f65277ae23b7f813aa00ec81
zirris512 <zirris512@gmail.com>
2023-04-28 19:39:29 -0500

    make section headers stretch across screen

--- src/components/SectionWrapper/SectionWrapper.jsx
+++ src/components/SectionWrapper/SectionWrapper.jsx
@@ -1,12 +1,12 @@
 import styles from "./SectionWrapper.module.scss";
 
 const SectionWrapper = (props) => {
-    return (
-        <section className={styles.wrapper} id={props.id}>
-            <h1 className={styles.header}>{props.title}</h1>
-            {props.children}
-        </section>
-    );
+	return (
+		<section className={styles.wrapper} id={props.id}>
+			<h1 className={styles.header}>{props.title}</h1>
+			<div className={styles.container}>{props.children}</div>
+		</section>
+	);
 };
 
 export default SectionWrapper;
