3cee58c007fd70442c3c862dcd6135f46e3d3d60
zirris512 <zirris512@gmail.com>
2023-05-04 20:31:09 -0500

    add card grid layout to projects section

--- src/Sections/ProjectsSection.jsx
+++ src/Sections/ProjectsSection.jsx
@@ -1,17 +1,18 @@
 import SingleProject from "../components/SingleProject/SingleProject";
 import SectionWrapper from "../components/SectionWrapper/SectionWrapper";
 import { projects } from "../data/data.json";
+import styles from "./ProjectsSection.module.scss";
 
 const ProjectsSection = () => {
-    return (
-        <SectionWrapper title="Projects" id="projects">
-            <div>
-                {projects.map((project) => (
-                    <SingleProject projectData={project} key={project.id} />
-                ))}
-            </div>
-        </SectionWrapper>
-    );
+	return (
+		<SectionWrapper title="Projects" id="projects">
+			<div className={styles.projects}>
+				{projects.map((project) => (
+					<SingleProject projectData={project} key={project.id} />
+				))}
+			</div>
+		</SectionWrapper>
+	);
 };
 
 export default ProjectsSection;
--- src/components/SingleProject/SingleProject.jsx
+++ src/components/SingleProject/SingleProject.jsx
@@ -5,44 +5,46 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import ImageLoading from "../ImageLoading/ImageLoading";
 
 const SingleProject = ({ projectData }) => {
-    const technologiesString = projectData.technologies.join(", ");
+	const technologiesString = projectData.technologies.join(", ");
 
-    return (
-        <article className={styles.projectContainer}>
-            <div className={styles.projectImgContainer}>
-                <ImageLoading
-                    image={projectData.imgUrl}
-                    imgAlt={projectData.imgAlt}
-                    imgClass={styles.projectImg}
-                />
-            </div>
-            <div className={styles.projectContent}>
-                <div>
-                    <h2>{projectData.name}</h2>
-                    <p className={styles.techList}>{technologiesString}</p>
-                </div>
-                <div>
-                    <p>{projectData.description}</p>
-                    <a
-                        href={projectData.deployedUrl}
-                        target="_blank"
-                        aria-label={`${projectData.name} deployment link`}
-                        className={styles.projectLink}
-                    >
-                        <FontAwesomeIcon icon={faGlobe} />
-                    </a>
-                    <a
-                        href={projectData.github}
-                        target="_blank"
-                        aria-label={`${projectData.name} github link`}
-                        className={styles.projectLink}
-                    >
-                        <FontAwesomeIcon icon={faGithub} />
-                    </a>
-                </div>
-            </div>
-        </article>
-    );
+	return (
+		<article className={styles.projectContainer}>
+			<div className={styles.projectImgContainer}>
+				<ImageLoading
+					image={projectData.imgUrl}
+					imgAlt={projectData.imgAlt}
+					imgClass={styles.projectImg}
+				/>
+			</div>
+			<div className={styles.projectContent}>
+				<div className={styles.projectLinks}>
+					<a
+						href={projectData.deployedUrl}
+						target="_blank"
+						title={projectData.deployedUrl}
+						aria-label={`${projectData.name} deployment link`}
+						className={styles.projectLink}
+					>
+						<FontAwesomeIcon icon={faGlobe} />
+					</a>
+					<a
+						href={projectData.github}
+						target="_blank"
+						title={projectData.github}
+						aria-label={`${projectData.name} github link`}
+						className={styles.projectLink}
+					>
+						<FontAwesomeIcon icon={faGithub} />
+					</a>
+				</div>
+				<div>
+					<h2>{projectData.name}</h2>
+					<p className={styles.techList}>{technologiesString}</p>
+				</div>
+				<p>{projectData.description}</p>
+			</div>
+		</article>
+	);
 };
 
 export default SingleProject;
